714ceb424cf11601ffb4b5a9bc599b15965f723e
Dexametasona <machicadogomezalexander@gmail.com>
2022-12-29 10:47:52 -0500

    push para el docente

--- src/app/elementos/dado/dado.component.ts
+++ src/app/elementos/dado/dado.component.ts
@@ -6,9 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dado.component.scss']
 })
 export class DadoComponent implements OnInit {
+  valor!:number
 
-  constructor() { }
+  lanzar(){
+    let i=7;
 
+    while(i>6 || i==0){
+      i=Math.trunc(Math.random()*10)
+      this.valor=i
+    }
+  }
   ngOnInit(): void {
   }
 
